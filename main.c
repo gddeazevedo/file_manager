80e31b76a271422f3c5e03add965cc8361e0f821
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2022-12-28 21:35:23 -0300

    starting cli

--- main.c
+++ main.c
@@ -1,23 +1,6 @@
-#include "src/memory/memory.h"
-#include "src/file_manager/file_manager.h"
+#include "src/cli/cli.h"
 
 int main() {
-    // run_cli();
-
-    Memory* mem = newMemory();
-
-    insert_file(mem); // test2
-    insert_file(mem); // test1
-    remove_from(mem, "test2.txt");
-    insert_file(mem); // test3
-
-    show_file(mem);
-
-    // for (int i = 0; i < MEMORY_LENGTH; i++) {
-    //     if (mem->ram[i] != NULL) {
-    //         printf("%d - %s - %d\n", i, mem->ram[i]->file_name, mem->ram[i]->next);
-    //     }
-    // }
-
+    run_cli();
     return 0;
 }
