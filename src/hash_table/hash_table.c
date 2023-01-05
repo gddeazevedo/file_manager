e2dd8f88488b51e6c46b08fddd8e0f3a4862b246
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-01-04 23:45:14 -0300

    implementing hash table

---
+++ src/hash_table/hash_table.c
@@ -0,0 +1,50 @@
+#include "hash_table.h"
+
+
+static uint hash(char* key) {
+    uint hash_val = 0;
+    int len = strlen(key);
+
+    for (int i = 0; i < len; i++) {
+        hash_val += key[i];
+        hash_val *= key[i];
+        hash_val *= hash_val;
+    }
+
+    hash_val %= TABLE_SIZE;
+
+    return hash_val;
+}
+
+
+static HashNode* search(char* key) {
+    HashNode* node = table[hash(key)];
+
+    while (node != NULL && strcmp(node->key, key) != 0) {
+        node = node->next;
+    }
+
+    return node;
+}
+
+int get(char* key) {
+    HashNode* node = search(key);
+    if (node == NULL) return -1;
+    return node->item;
+}
+
+bool put(char* key, int item) {
+    HashNode* node = search(key);
+
+    if (node == NULL) {
+        node = (HashNode*) malloc(sizeof(HashNode));
+        strcpy(node->key, key);
+        node->item = item;
+        uint index = hash(key);
+        node->next = table[index];
+        table[index] = node;
+        return true;
+    }
+
+    return false;
+}
---
+++ src/hash_table/hash_table.h
@@ -0,0 +1,28 @@
+#ifndef __hash_table_h
+#define __hash_table_h
+
+#include <stdbool.h>
+#include <string.h>
+
+#define TABLE_SIZE 30
+#define KEY_LEN 50
+
+typedef unsigned int uint;
+
+typedef struct __hash_node {
+    char key[KEY_LEN];
+    uint item;
+    struct __hash_node* next;
+} HashNode;
+
+
+
+static HashNode* table[TABLE_SIZE];
+
+
+static uint hash(char* key);
+static HashNode* search(char* key);
+int get(char* key);
+bool put(char* key, int item);
+
+#endif
