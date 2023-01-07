70b88f8860fb222c6456d2aeb3b55abb9025a276
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-01-06 22:31:18 -0300

    adding helpers

---
+++ src/helpers/helpers.c
@@ -0,0 +1,9 @@
+#include "helpers.h"
+
+
+void pause_program() {
+    char aux;
+    printf("\nPressione ENTER para voltar.");
+    scanf("%c", &aux);
+    getchar();
+}
---
+++ src/helpers/helpers.h
@@ -0,0 +1,8 @@
+#ifndef __helpers_h
+#define __helpers_h
+
+#include <stdio.h>
+
+void pause_program();
+
+#endif
